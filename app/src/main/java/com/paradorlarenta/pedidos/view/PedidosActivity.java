f9320dcc39b9b714445e60bedad339f57ae891b3
sbpinilla <sbpinilla@hotmail.com>
2018-03-06 11:52:14 -0500

    add layout pedidos

---
+++ app/src/main/java/com/paradorlarenta/pedidos/view/PedidosActivity.java
@@ -0,0 +1,15 @@
+package com.paradorlarenta.pedidos.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.paradorlarenta.pedidos.R;
+
+public class PedidosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pedidos);
+    }
+}
